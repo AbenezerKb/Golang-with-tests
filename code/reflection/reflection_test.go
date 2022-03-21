3c66c865206fb47a85b8bd2557387b0b2422299e
unknown <aben.jacob3@gmail.com>
2022-03-21 12:29:36 +0300

    reflection test

---
+++ code/reflection/reflection_test.go
@@ -0,0 +1,27 @@
+package reflection
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestReflection(t *testing.T) {
+
+	t.Run("with function", func(t *testing.T) {
+		aFunction := func() (Profile, Profile) {
+			return Profile{33, "Berlin"}, Profile{34, "Katowice"}
+		}
+
+		var got []string
+		want := []string{"Berlin", "Katowice"}
+
+		walk(aFunction, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+
+}
